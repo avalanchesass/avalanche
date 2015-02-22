b19609d5027536529703f49aa69db3deec82e3a1
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-02-22 10:23:38 +0100

    added custom twig templates to the styleguide

--- styleguide/index.php
+++ styleguide/index.php
@@ -8,6 +8,7 @@ $styleguide = new cultusrego(array(
   'source' => '../css/avalanche.css',
   'title' => 'avalanche Styleguide',
   'description' => 'SASS / CSS Framework',
+  'template_folder' => __DIR__ . '/templates',
   'twig_cache' => FALSE,
 ));
 $styleguide->render();
