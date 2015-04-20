900a89df13aa11c29269a3320a1e035bc9b9bb72
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-04-20 19:40:40 +0200

    updated gulp buildproces, removed package loading

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var sourcemaps       = require('gulp-sourcemaps');
 gulp.task('styles', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sourcemaps.init())
-      .pipe(sass({ 'precision': 7, 'includePaths': ['bower_components'], 'errLogToConsole': true }))
+      .pipe(sass({ precision: 7, includePaths: ['bower_components'], errLogToConsole: true }))
       .pipe(autoprefixer())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('css'));
@@ -36,56 +36,4 @@ gulp.task('watch', function () {
 // Default
 gulp.task('default', function () {
   gulp.start('watch');
-});
-
-/**
- * Component loading
- *
- * Usage:
- * gulp avalanche-extend --utility name
- * gulp avalanche-extend --object name
- */
-var download  = require('gulp-download');
-var httpcheck = require('httpcheck');
-var args      = require('yargs').argv;
-
-gulp.task('avalanche-extend', function () {
-  var branch = 'release-1.5';
-  var baseUrl = 'https://cdn.rawgit.com/maoberlehner/avalanche_{type}_{arg}/' + branch + '/_{arg}.scss';
-
-  var getExtension = function (extensionType, extensionName, extensionUrl, extensionDestination) {
-    httpcheck({
-      url: extensionUrl,
-      log: function () {},
-      check: function(res) {
-        if (res && res.statusCode === 404) {
-          console.error(extensionType + ' "' + extensionName + '" not found');
-          return true;
-        }
-        download(extensionUrl).pipe(gulp.dest(extensionDestination));
-        return false;
-      }
-    },
-    function (err) {
-      if (err) {
-        throw err;
-      }
-    });
-  };
-
-  var utilityName;
-  var utilityUrl;
-  if (args.utility) {
-    utilityName = args.utility;
-    utilityUrl = baseUrl.replace('{type}', 'utility').replace(/\{arg\}/g, utilityName);
-    getExtension('Utility', utilityName, utilityUrl, 'scss/utility/');
-  }
-
-  var objectName;
-  var objectUrl;
-  if (args.object) {
-    objectName = args.object;
-    objectUrl = baseUrl.replace('{type}', 'object').replace(/\{arg\}/g, objectName);
-    getExtension('Utility', objectName, objectUrl, 'scss/object/');
-  }
 });
