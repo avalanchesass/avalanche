785e10b1980a2e6491699c4806bd2cf67b3503aa
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-04-21 19:40:52 +0200

    implemented auto injection of packages installed via bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 // Load plugins
 var gulp             = require('gulp');
 var autoprefixer     = require('gulp-autoprefixer');
+var bower            = require('gulp-bower');
 var csso             = require('gulp-csso');
+var inject           = require('gulp-inject');
 var livereload       = require('gulp-livereload');
 var rename           = require('gulp-rename');
 var sass             = require('gulp-sass');
@@ -11,7 +13,7 @@ var sourcemaps       = require('gulp-sourcemaps');
 gulp.task('styles', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sourcemaps.init())
-      .pipe(sass({ precision: 7, includePaths: ['bower_components'], errLogToConsole: true }))
+      .pipe(sass({ precision: 7, errLogToConsole: true }))
       .pipe(autoprefixer())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('css'));
@@ -26,6 +28,52 @@ gulp.task('minify', ['styles'], function () {
     }))
     .pipe(gulp.dest('css'))
     .pipe(livereload());
+});
+
+// Bower
+gulp.task('bower', function () {
+  gulp.start('inject');
+  return bower();
+});
+
+// Inject
+gulp.task('inject', ['bower'], function () {
+  // Inject packages in avalanche.scss
+  var functions  = gulp.src('bower_components/avalanche_function_*/scss/*.scss', { read: false });
+  var utilities  = gulp.src('bower_components/avalanche_utility_*/scss/*.scss', { read: false });
+  var objects    = gulp.src('bower_components/avalanche_object_*/scss/*.scss', { read: false });
+  var components = gulp.src('bower_components/avalanche_component_*/scss/*.scss', { read: false });
+
+  return gulp.src('scss/avalanche.scss')
+    .pipe(inject(functions, {
+      starttag: '/** inject:function **/',
+      endtag: '/** endinject **/',
+      transform: function (filepath, file, i, length) {
+        return '@import \'' + filepath.substring(1) + '\';';
+      }
+    }))
+    .pipe(inject(utilities, {
+      starttag: '/** inject:utility **/',
+      endtag: '/** endinject **/',
+      transform: function (filepath, file, i, length) {
+        return '@import \'' + filepath.substring(1) + '\';';
+      }
+    }))
+    .pipe(inject(objects, {
+      starttag: '/** inject:object **/',
+      endtag: '/** endinject **/',
+      transform: function (filepath, file, i, length) {
+        return '@import \'' + filepath.substring(1) + '\';';
+      }
+    }))
+    .pipe(inject(components, {
+      starttag: '/** inject:component **/',
+      endtag: '/** endinject **/',
+      transform: function (filepath, file, i, length) {
+        return '@import \'' + filepath.substring(1) + '\';';
+      }
+    }))
+    .pipe(gulp.dest('scss'));
 });
 
 // Watch
