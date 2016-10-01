9b46ddff62e1e950b30b8aab7fcba4495e804e8b
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-10-01 19:01:05 +0200

    Global testing

---
+++ scripts/test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+case $key in
+    -p|--package)
+    PACKAGE="$2"
+    shift
+    ;;
+    *)
+    ;;
+esac
+shift
+done
+
+for f in packages/*; do
+  if [ -n "$PACKAGE" ] && [ `basename $f` != "$PACKAGE" ]; then
+    continue
+  fi
+
+  if [ -d "$f/test" ]; then
+    # Build CSS
+    node_modules/node-sass/bin/node-sass --importer node_modules/node-sass-magic-importer "$f/test/test.scss" | node_modules/postcss-cli/bin/postcss -u autoprefixer -o "$f/test/tmp/test.css"
+    # Build HTML
+    BODY=$(cat "$f/test/body.html" | tr '\r\n' ' '); sed "s~{{ body }}~${BODY}~g" test/test.html.template > "$f/test/tmp/test.html"
+    # Test
+    ( cd "$f" && ../../node_modules/backstopjs/cli/index.js test --configPath=../../backstop.json )
+    # Cleanup
+    rm -Rf "$f/test/tmp"
+  fi
+done
