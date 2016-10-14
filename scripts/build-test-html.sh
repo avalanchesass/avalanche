3d0773033814054e1eaeb51a6b3f8d83f4322386
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-10-14 16:56:24 +0200

    Removed website generation feature. Update tests / rename test hsb file

--- scripts/build-test-html.sh
+++ scripts/build-test-html.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$DIR/_build-test-parameter.sh"
 
-BODY=$(cat "packages/$PACKAGE/test/body.hbs")
+BODY=$(cat "packages/$PACKAGE/test/test.hbs")
 echo $(cat test/test.hbs) | node_modules/handlebars-cmd/index.js --body "$BODY" > "packages/$PACKAGE/test/tmp/test.html"
