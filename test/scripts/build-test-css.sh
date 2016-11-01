0a968063d74e5c0036ee891c95a86682e07bb6d4
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-11-01 15:46:42 +0100

    Configure the autoprefixer building the test css files to build for safari >= 4 for maximum compatibility (this prevents problems with flexbox)

--- test/scripts/build-test-css.sh
+++ test/scripts/build-test-css.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$DIR/_build-test-parameter.sh"
 
-node_modules/node-sass/bin/node-sass --importer node_modules/node-sass-magic-importer "packages/$PACKAGE/test/test.scss" | node_modules/postcss-cli/bin/postcss -u autoprefixer -o "packages/$PACKAGE/test/tmp/test.css"
+node_modules/node-sass/bin/node-sass --importer node_modules/node-sass-magic-importer "packages/$PACKAGE/test/test.scss" | node_modules/postcss-cli/bin/postcss -u autoprefixer --autoprefixer.browsers "safari >= 4" -o "packages/$PACKAGE/test/tmp/test.css"
