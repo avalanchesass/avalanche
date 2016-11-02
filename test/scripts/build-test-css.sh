6e9d29d9f0f4310eae64b8148d62929c312c0a59
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-11-02 19:46:31 +0100

    Update the node-sass-magic-importer dependency and change the script execution for the test css build step, to cd in the package directory so the CWD ist the package directory for node module resolving

--- test/scripts/build-test-css.sh
+++ test/scripts/build-test-css.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$DIR/_build-test-parameter.sh"
 
-node_modules/node-sass/bin/node-sass --importer node_modules/node-sass-magic-importer "packages/$PACKAGE/test/test.scss" | node_modules/postcss-cli/bin/postcss -u autoprefixer --autoprefixer.browsers "safari >= 4" -o "packages/$PACKAGE/test/tmp/test.css"
+( cd "packages/$PACKAGE" && ../../node_modules/node-sass/bin/node-sass --importer ../../node_modules/node-sass-magic-importer/dist/cli.js "test/test.scss" | ../../node_modules/postcss-cli/bin/postcss -u autoprefixer --autoprefixer.browsers "safari >= 4" -o "test/tmp/test.css" )
