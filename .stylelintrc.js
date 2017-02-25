f5081008fc3d78222248ede677257f23116fc619
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-25 18:26:03 +0100

    Remove obsolete csscomb setting file.
    Property order is already defined in the stylint base config.

--- .stylelintrc.js
+++
@@ -1,8 +0,0 @@
-const declarationBlockPropertiesOrder = require(`./.csscomb.json`)[`sort-order`][0];
-
-module.exports = {
-  extends: `stylelint-config-modularis`,
-  rules: {
-    'declaration-block-properties-order': declarationBlockPropertiesOrder,
-  },
-};
